d04869da93297ea04b03906f669518ae8862aab9
Asheet Sinvhal <iamAsheet@users.noreply.github.com>
2019-01-13 00:47:02 +0530

    Update testtraderportal.js

--- testtraderportal.js
+++ testtraderportal.js
@@ -4,7 +4,7 @@
 
  //var team_data = {"1": ["Affluentia", 19909.610000000033], "2": ["Sparrow", 50000.0], "3": ["Valu-ed", 606212.3499999999], "4": ["The Brainaics", 392853.30000000005], "5": ["Addicts", 1327163.0], "6": ["Enigma", 632385.9000000001], "7": ["Think Big", 671726.9], "8": ["White collar crew", 132343.3], "9": ["Revolution", 832237.5], "10": ["Bright sparks", 262476.19999999995], "11": ["Fingeeks", 500593.5], "12": ["wizards", 374245.86], "13": ["Midnight warriors", 1844.6600000000326], "14": ["Achievers", 18007.300000000047], "15": ["Backbencher", 39599.39999999998], "16": ["Super Saiyan", 692623.6]};
  var shares_data = [["AUSTRALIA", "ANZ Group", "ANZ Group", 531.5024121295662, 1509], ["JAPAN", "Aerozone", "Aerozone", 2615.0, 2280], ["CHINA", "Alibaba", "Alibaba", 1609.8985679747072, 1910], ["USA", "Apple", "Apple", 2668.8899990642244, 2304], ["INDIA", "BHEL", "BHEL", 88.66002961856086, 764], ["AUSTRALIA", "BHP Billiton", "BHP Billiton", 1161.2500000036819, 1650], ["GERMANY", "BMW", "BMW AG", 941.2, 1960], ["ENGLAND", "Barclays", "Barclays", 1341.3, 2010], ["ENGLAND", "Brirish Airways", "British Airways", 1923.0, 2175], ["GERMANY", "Continental", "Continental", 1736.3639674699116, 2245], ["GERMANY", "Deutsche", "Deutsche AG", 1897.999999708374, 1960], ["ENGLAND", "Diageo", "Diageo", 1836.899401848831, 2235], ["USA", "Drone CO", "Drone CO", 2451.042508829539, 1944], ["USA", "Google", "Google", 382.0, 70], ["INDIA", "ICICI", "ICICI Bank", 165.69526690517813, 1355], ["INDIA", "Idea", "Idea Celluar", 421.3, 1778], ["CHINA", "Jigzena Ltd", "Jigzena Ltd", 904.0, 2330], ["AUSTRALIA", "K&S Corp.", "K&S Corporation Ltd.", 329.30000000000007, 1990], ["AUSTRALIA", "Karmanta Bank", "Karmanta Bank", 849.0, 1650], ["JAPAN", "Krayota Motor", "Krayota Motor Corp", 2149.0, 2210], ["USA", "Microsoft", "Microsoft Corp.", 1348.0, 1850], ["JAPAN", "Nippon Steel", "Nippon Steel", 1248.0, 1940], ["USA", "Petrologistics", "Petrologistic LP", 1437.5399999999984, 2080], ["INDIA", "Reliance", "Reliance Industries Limited", 1242.2500000112082, 1965], ["JAPAN", "Samurai Corp", "Samurai Corp", 1153.59, 2050], ["CHINA", "Sany Group", "Sany Group", 703.4000000000001, 1990], ["GERMANY", "Siemens", "Siemens AG", 450.3, 1340], ["CHINA", "Soft Bank", "Soft Bank", 400.8700000001181, 840], ["JAPAN", "Sony", "Sony", 1072.86, 1521], ["INDIA", "Sun Pharma", "Sun Pharma", 732.3799954811025, 1507], ["AUSTRALIA", "Telstra", "Telstra", 1289.9499647467978, 2158], ["ENGLAND", "Tesco", "Tesco", 999.66, 1680], ["ENGLAND", "Vodafone", "Vodafone", 1672.5799973146472, 1735], ["GERMANY", "Volkswagen", "Volkswagen AG", 1848.8986578554513, 2310], ["CHINA", "Xiomi", "Xiomi", 1410.0, 2050]];
- var country_data = {"AUSTRALIA": ["ANZ Group", "BHP Billiton", "K&S Corporation Ltd.", "Karmanta Bank", "Telstra"], "ENGLAND": ["Barclays", "British Airways", "Diageo", "Tesco", "Vodafone"], "USA": ["Apple", "Drone CO", "Google", "Microsoft Corp.", "Petrologistic LP"], "INDIA": ["BHEL", "ICICI Bank", "Idea Celluar", "Reliance Industries Limited", "Sun Pharma"], "CHINA": ["Alibaba", "Jigzena Ltd", "Sany Group", "Soft Bank", "Xiomi"], "GERMANY": ["BMW AG", "Continental", "Deutsche AG", "Siemens AG", "Volkswagen AG"], "JAPAN": ["Aerozone", "Krayota Motor Corp", "Nippon Steel", "Samurai Corp", "Sony"]}
+ var country_data = {"AUSTRALIA": ["ANZ", "BHP", "KAS", "KRMB", "TELS"], "ENGLAND": ["BCL", "BAR", "DAG", "TSCO", "VODA"], "USA": ["APPL", "DRCO", "GOOG", "MSFT", "PLP"], "INDIA": ["BHEL", "ICIC", "IDEA", "RIL", "SUN"], "CHINA": ["ALI", "JIGZ", "SANG", "SOFT", "XOMI"], "GERMANY": ["BMW", "CONT", "DESH", "SMNS", "VWG"], "JAPAN": ["ARZ", "KMC", "NPPO", "SAM", "SONY"]};
  
  var country;
  var stocks;
@@ -111,6 +111,7 @@
     	 for(var k=1; k < portfolio_data.length; k += 1) {
              if(portfolio_data[k][0] == team_id){
 		main_p.innerHTML += '<div style="display: table-row">' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + portfolio_data[k][1] + '</div>' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + portfolio_data[k][2] +'</div>' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + portfolio_data[k][4] +'</div>'+'</div>';
+	        break;
 	     }
    	 }    
    	 main_p.innerHTML += '</div>';
@@ -133,7 +134,7 @@
  
  function showNotif(text, background="white",color="black") {
 	  if(document.getElementById('notif').style.display == 'block') {
-	   setTimeout(function(){showNotif(text, background)},2000);
+	    setTimeout(function(){showNotif(text, background)},2000);
 	  } else {
 	   document.getElementById('notif').innerHTML = text.toUpperCase();
 	   document.getElementById('notif').style.background = background;
@@ -145,16 +146,15 @@
 	 
 
  function updateMarketPrice() {
-  stock = document.getElementById('main').value;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById('price').value = Math.round(parseFloat(this.responseText.split('|')[0])*100)/100;;
-    document.getElementById('quantity').setAttribute('placeholder','MAX BUY '+this.responseText.split('|')[1]);
-   }
-  };
-  xhttp.open("GET", "mrkt?stock="+encodeURIComponent(stock), true);
-  xhttp.send();
+  stockid = document.getElementById('main').value;
+  for(var k=1; k < stocks.length ; k+=1){
+	if( stocks[k][0] == stockid) {
+	    var price = stocks[k][4];
+	    var quantity = stocks[k][2];
+	    document.getElementById('price').value = Math.round(parseFloat(price)*100)/100;;
+	    document.getElementById('quantity').setAttribute('placeholder','MAX BUY '+quantity);
+	    break;
+	} 
  }
  
  
