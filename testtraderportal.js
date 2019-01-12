252782894da2fba3ddead50a790f51984a28bbab
Asheet Sinvhal <iamAsheet@users.noreply.github.com>
2019-01-12 16:31:45 +0530

    Update testtraderportal.js

--- testtraderportal.js
+++ testtraderportal.js
@@ -6,6 +6,9 @@
  var shares_data = [["AUSTRALIA", "ANZ Group", "ANZ Group", 531.5024121295662, 1509], ["JAPAN", "Aerozone", "Aerozone", 2615.0, 2280], ["CHINA", "Alibaba", "Alibaba", 1609.8985679747072, 1910], ["USA", "Apple", "Apple", 2668.8899990642244, 2304], ["INDIA", "BHEL", "BHEL", 88.66002961856086, 764], ["AUSTRALIA", "BHP Billiton", "BHP Billiton", 1161.2500000036819, 1650], ["GERMANY", "BMW", "BMW AG", 941.2, 1960], ["ENGLAND", "Barclays", "Barclays", 1341.3, 2010], ["ENGLAND", "Brirish Airways", "British Airways", 1923.0, 2175], ["GERMANY", "Continental", "Continental", 1736.3639674699116, 2245], ["GERMANY", "Deutsche", "Deutsche AG", 1897.999999708374, 1960], ["ENGLAND", "Diageo", "Diageo", 1836.899401848831, 2235], ["USA", "Drone CO", "Drone CO", 2451.042508829539, 1944], ["USA", "Google", "Google", 382.0, 70], ["INDIA", "ICICI", "ICICI Bank", 165.69526690517813, 1355], ["INDIA", "Idea", "Idea Celluar", 421.3, 1778], ["CHINA", "Jigzena Ltd", "Jigzena Ltd", 904.0, 2330], ["AUSTRALIA", "K&S Corp.", "K&S Corporation Ltd.", 329.30000000000007, 1990], ["AUSTRALIA", "Karmanta Bank", "Karmanta Bank", 849.0, 1650], ["JAPAN", "Krayota Motor", "Krayota Motor Corp", 2149.0, 2210], ["USA", "Microsoft", "Microsoft Corp.", 1348.0, 1850], ["JAPAN", "Nippon Steel", "Nippon Steel", 1248.0, 1940], ["USA", "Petrologistics", "Petrologistic LP", 1437.5399999999984, 2080], ["INDIA", "Reliance", "Reliance Industries Limited", 1242.2500000112082, 1965], ["JAPAN", "Samurai Corp", "Samurai Corp", 1153.59, 2050], ["CHINA", "Sany Group", "Sany Group", 703.4000000000001, 1990], ["GERMANY", "Siemens", "Siemens AG", 450.3, 1340], ["CHINA", "Soft Bank", "Soft Bank", 400.8700000001181, 840], ["JAPAN", "Sony", "Sony", 1072.86, 1521], ["INDIA", "Sun Pharma", "Sun Pharma", 732.3799954811025, 1507], ["AUSTRALIA", "Telstra", "Telstra", 1289.9499647467978, 2158], ["ENGLAND", "Tesco", "Tesco", 999.66, 1680], ["ENGLAND", "Vodafone", "Vodafone", 1672.5799973146472, 1735], ["GERMANY", "Volkswagen", "Volkswagen AG", 1848.8986578554513, 2310], ["CHINA", "Xiomi", "Xiomi", 1410.0, 2050]];
  var country_data = {"AUSTRALIA": ["ANZ Group", "BHP Billiton", "K&S Corporation Ltd.", "Karmanta Bank", "Telstra"], "ENGLAND": ["Barclays", "British Airways", "Diageo", "Tesco", "Vodafone"], "USA": ["Apple", "Drone CO", "Google", "Microsoft Corp.", "Petrologistic LP"], "INDIA": ["BHEL", "ICICI Bank", "Idea Celluar", "Reliance Industries Limited", "Sun Pharma"], "CHINA": ["Alibaba", "Jigzena Ltd", "Sany Group", "Soft Bank", "Xiomi"], "GERMANY": ["BMW AG", "Continental", "Deutsche AG", "Siemens AG", "Volkswagen AG"], "JAPAN": ["Aerozone", "Krayota Motor Corp", "Nippon Steel", "Samurai Corp", "Sony"]}
  
+ var country;
+ var stocks;
+ var teams;
  function initClient() {
      var API_KEY = 'AIzaSyCr8id8gmmgCSr28P3PxWNiKvga6im2P1s';  // TODO: Update placeholder with desired API key.
 
@@ -71,6 +74,9 @@
        // TODO: Change code below to process the `response` object:
        console.log(response.result);
         var all_data = response.result;
+	country = all_data.valueRanges[0].values;
+	stocks =  all_data.valueRanges[1].values;
+	teams = all_data.valueRanges[2].values;
      }, function(reason) {
        console.error('error: ' + reason.result.error.message);
      });
