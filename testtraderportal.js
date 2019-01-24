b1b2602f17ec6dea9fd1e38ab26a89fdf1609ea6
Asheet Sinvhal <iamAsheet@users.noreply.github.com>
2019-01-25 01:05:02 +0530

    Update testtraderportal.js

--- testtraderportal.js
+++ testtraderportal.js
@@ -114,6 +114,7 @@
 	     if(portfolio_data[k][0] == team_id){
 		var current_value = Math.round(parseFloat(portfolio_data[k][4])*100)/100 ; 
 		main_p.innerHTML += '<div style="display: table-row">' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + portfolio_data[k][1] + '</div>' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + portfolio_data[k][2] +'</div>' + '<div style="display: table-cell;padding: 4px;border: 1px solid black;">' + current_value +'</div>'+'</div>';
+	    	break;
 	     }
 	 }    
 	 main_p.innerHTML += '</div>';
