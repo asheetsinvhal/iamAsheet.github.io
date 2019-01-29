6e23c86252bdca8ecae834ce27d3aeec9bcf254b
Asheet Sinvhal <iamAsheet@users.noreply.github.com>
2019-01-29 18:33:49 +0530

    Update stockdata.js

--- js/stockdata.js
+++ js/stockdata.js
@@ -239,7 +239,7 @@ function updatePriceData2() {
 	      else if(curr > old) {
 					 stk_table.rows[i].cells[2].innerHTML = curr;
 					 stk_table.rows[i].cells[3].innerHTML = '&uarr; ' + Math.round((curr-old)*100)/100;
-					 stk_table.rows[i].cells[3].style.background = '#2e7136';
+					 stk_table.rows[i].cells[3].style.background = '#2e762e';
 					 stk_table.rows[i].cells[3].style.color = '#f0f0f5';
 					 //stk_table.rows[i].cells[3].style.background = '#323C50';
 					 //stk_table.rows[i].cells[3].style.color = '#47d147';
